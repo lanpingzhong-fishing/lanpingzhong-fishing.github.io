9c19cf7f272e49dc4aaf487ce70105435310c1dc
lanpingzhong-fishing <161913146+lanpingzhong-fishing@users.noreply.github.com>
2024-03-13 22:41:19 +0800

    ...

---
+++ cssjs/a.js
@@ -0,0 +1,34 @@
+function jump() {
+	var currentPath = window.location.pathname;
+	var newUrl = "https://lyuwenhan.github.io" + currentPath;
+	window.location.href = newUrl;
+	return false;
+}
+function get() {
+	$.getJSON('https://lanpingzhong-fishing.github.io/file/fishing.json', function(jsonData) {
+		console.log('Received data:', jsonData);
+		var fileList = jsonData.file;
+		var contributionElement = document.querySelector('.contribution');
+		fileList.forEach(function(file) {
+		  var linkElement = document.createElement('a');
+		  linkElement.textContent = file.name;
+		  linkElement.href = file.path;
+		  contributionElement.parentNode.insertBefore(linkElement, contributionElement);
+		  contributionElement.parentNode.insertBefore(document.createElement('br'), contributionElement);
+		});
+	})
+}
+function get2() {
+	$.getJSON('https://lanpingzhong-fishing.github.io/file/fishing_delete.json', function(jsonData) {
+		console.log('Received data:', jsonData);
+		var fileList = jsonData.file;
+		var contributionElement = document.querySelector('.contribution');
+		fileList.forEach(function(file) {
+		  var linkElement = document.createElement('a');
+		  linkElement.textContent = file.name;
+		  linkElement.href = file.path;
+		  contributionElement.parentNode.insertBefore(linkElement, contributionElement);
+		  contributionElement.parentNode.insertBefore(document.createElement('br'), contributionElement);
+		});
+	})
+}
