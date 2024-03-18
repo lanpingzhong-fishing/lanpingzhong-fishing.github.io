400bcaeb1f0d1a41a7e3c0dd9562066f61e4728b
lanpingzhong-fishing <161913146+lanpingzhong-fishing@users.noreply.github.com>
2024-03-18 22:42:02 +0800

    ...

--- cssjs/a.js
+++ cssjs/a.js
@@ -4,12 +4,6 @@ function jump() {
 	window.location.href = newUrl;
 	return false;
 }
-function jump() {
-	var currentPath = window.location.pathname;
-	var newUrl = "https://lyuwenhan.github.io" + currentPath;
-	window.location.href = newUrl;
-	return false;
-}
 function remove_to_back(url) {
     var segments = url.split('/');
     if (segments.length <= 1) {
@@ -22,6 +16,6 @@ function remove_to_back(url) {
 function jump_back() {
 	var currentPath = window.location.href;
 	console.log(remove_to_back(remove_to_back(currentPath)));
-	window.location.href = remove_to_back(remove_to_back(currentPath));
+	window.location.href = "https://lanpingzhong-fishing.github.io" + remove_to_back(remove_to_back(currentPath));
 	return false;
 }
